26a3b22f4a1c19585c544c3fceee88c77a91ddc5
Varunappalla93 <an.varun1993@gmail.com>
2025-04-08 16:31:26 +0530

    terraform conditions functions loops

--- locals/count.tf
+++ locals/count.tf
@@ -2,11 +2,12 @@ resource "aws_instance" "web" {
   #   count         = 11 # count.index is special variable given by terraform
   count         = length(var.instance_names)
   ami           = var.ami_id
-  instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql" || var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
+  instance_type = local.instance_type
   tags = {
     Name = var.instance_names[count.index]
   }
 }
+
 
 resource "aws_route53_record" "www" {
   #   count   = 11
@@ -15,5 +16,5 @@ resource "aws_route53_record" "www" {
   name    = "${var.instance_names[count.index]}.${var.domain_name}" # interpolation
   type    = "A"
   ttl     = 1
-  records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
+  records = [local.ip]
 }
