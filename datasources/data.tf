b459d3f8962fa06c2da41892b74ca0d14c8695a8
Varunappalla93 <an.varun1993@gmail.com>
2025-04-15 23:51:17 +0530

    terraform data-sorces and dynamic

--- datasources/data.tf
+++ datasources/data.tf
@@ -20,24 +20,24 @@ data "aws_ami" "centos8" {
 
 
 
-data "aws_ami" "aws-linux-2"{
-    owners = ["amazon"]
-    most_recent      = true
+data "aws_ami" "aws-linux-2" {
+  owners      = ["amazon"]
+  most_recent = true
 
-    filter {
-        name   = "name"
-        values = ["amzn2-ami-kernel-5.10-hvm-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*"]
+  }
 
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 data "aws_vpc" "default" {
