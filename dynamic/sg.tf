8cb80e29a324ade018ce533a9be6bb0dfe8cf82b
Varunappalla93 <an.varun1993@gmail.com>
2025-04-15 23:47:13 +0530

    terraform data-sorces and dynamic

--- dynamic/sg.tf
+++ dynamic/sg.tf
@@ -1,4 +1,4 @@
-# block inside resource getting repeated
+# block inside resource getting repeated is dynamic block
 resource "aws_security_group" "roboshop-all" { 
     name        = "dynamic-demo"
     description = "dynamic-demo"
