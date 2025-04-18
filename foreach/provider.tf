4153a802855e0fad08c16d4953e28f84d7d027f3
Varunappalla93 <an.varun1993@gmail.com>
2025-04-18 12:15:19 +0530

    s3 for remote state

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,7 +5,16 @@ terraform {
       version = "5.31.0" # AWS provider version, not terraform version
     }
   }
+
+  
+backend "s3" {
+    bucket = "daws76s-remote-state"
+    key    = "foreach"
+    region = "us-east-1"
+    dynamodb_table = "daws76s-locking"
+  }
 }
+
 
 provider "aws" {
   region = "us-east-1"
